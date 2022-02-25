32742f4405d918b126c6897f424978a5ec3ba388
Adc <82343598+aaldecas@users.noreply.github.com>
2022-02-25 10:36:33 -0500

    Add files via upload

---
+++ Ejercicios Adicionales Lab Modulo 04.sql
@@ -0,0 +1,27 @@
+
+-- Ejercicio 1
+-- Escriba una consulta que muestre el nombre, el Id del cliente y el total orden de los territorios que tienen ventas agrupelo por cada mes de cada año
+-- Name,CustomerID,SaleYear,SaleMonth,SubTotal,TaxAmt,TotalDue
+-- use las siguientes tablas: [Sales].[SalesTerritory] y Sales.SalesOrderHeader
+
+
+-- Ejercicio 2
+-- Escriba una consulta que contenga: El nombre del vendedor con mayores ventas por cada region, mes y año:
+-- Use [Sales].[SalesTerritory] y Sales.SalesOrderHeader,
+-- [HumanReso[Production].[ProductCategory]urces].[Employee],[Person].[Person],[Sales].[SalesTerritory] y Sales.SalesOrderHeader
+
+-- Ejercicio 3
+-- Escriba una consulta que contenga: El nombre de las categorías con menores ventas por cada region, mes y año:
+-- Use [Sales].[SalesTerritory] y Sales.SalesOrderHeader,
+-- [Sales].[SalesTerritory] y Sales.SalesOrderHeader,[Production].[ProductCategory]
+
+
+
+
+-- Ejercicio 4
+-- Escriba una consulta que muestre la razón mas importante por la cual se hacen ordenes de compra por año
+
+
+
+
+
